1ef6317e4221eff4f1267ca74610f4387cecfea2
TechnoGoku <bulatikilya@gmail.com>
2024-03-09 21:28:09 +0200

    update_4

--- hw_4.py
+++ hw_4.py
@@ -1,30 +1,47 @@
 from datetime import datetime, timedelta
+
+
 users_list = [
     {"name": "Joe McNuggets", "birthday": "1985.03.15"},
     {"name": "Frensis McChicken", "birthday": "1990.01.09"},
     {"name": "Gustav Antonio", "birthday": "1990.03.05"},
     {"name": "Carl Johnson", "birthday":"1968.07.10"},
     {"name": "Nico Bellic", "birthday": "1990.03.09"},
-    {"name": "Gabe Newell", "birthday": "1962.03.03"},
+    {"name": "Gabe Newell", "birthday": "1962.03.16"},
     {"name": "Sir Potato", "birthday": "2000.07.16"},
     {"name": "Mister Incognito", "brithday": "1999.03.12"}
 ]
 
-birthday = users_list("birthday")
-
-
 def get_upcoming_birthdays(users):
-    pass
+    prepared_users = []
+    for users in users_list:
+        try:
+            birthday_date = datetime.strftime(users_list["birthday"], "%Y.%m.%d").date
+            prepared_users.append({"name": users["name"], "birthday": birthday_date})
+        except:
+            print(f'Некоректна дата народження для користувача {users["name"]}')
+        return prepared_users
+    print(prepared_users)
+    print(birthday_date)
 
 
-for birthday in user_list:
-    birthday_date = datetime.strftime(birthday)
+def find_next_weekday(d, weekday: int):  # Функція для знаходження наступного заданого дня тижня після заданої дати
+    """
+     Ф-ція для знаходження наступного заданого дня тижня після заданої дати
+    :param d: datetime.date - початкова дата
+    :param weekday: int - день тижня від 0 (понеділок) до 6 (неділя)
+    :return:
+    """
+    days_ahead = weekday - d.weekday()  # Різниця між заданим днем тижня та днем тижня заданої дати
+    if days_ahead <= 0:  # Якщо день народження вже минув
+        days_ahead += 7  # Додаємо 7 днів, щоб отримати наступний тиждень
+    return d + timedelta(days=days_ahead)  # Повертаємо нову дату
 
 
+    
 
 get_upcoming_birthdays(users_list)
 today = datetime.today().date()
 
-print(birthday)
 print(today)
 print(users_list)
