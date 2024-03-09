b3cc39bc7c45d88305bc725c63ca81575a37e3f2
TechnoGoku <bulatikilya@gmail.com>
2024-03-09 20:57:56 +0200

    update_3

--- hw_4.py
+++ hw_4.py
@@ -1,24 +1,30 @@
 from datetime import datetime, timedelta
-users = [
-    {"name": "Joe McNuggets", "birthday": "1985.01.23"},
-    {"name": "Frensis McChicken", "birthday": "1990.01.27"},
+users_list = [
+    {"name": "Joe McNuggets", "birthday": "1985.03.15"},
+    {"name": "Frensis McChicken", "birthday": "1990.01.09"},
     {"name": "Gustav Antonio", "birthday": "1990.03.05"},
     {"name": "Carl Johnson", "birthday":"1968.07.10"},
-    {"name": "Nico Bellic", "birthday": "1990.03.07"},
-    {"name": "Gabe Newell", "birthday": "1962.10.03"},
+    {"name": "Nico Bellic", "birthday": "1990.03.09"},
+    {"name": "Gabe Newell", "birthday": "1962.03.03"},
     {"name": "Sir Potato", "birthday": "2000.07.16"},
-    {"name": "Mister Incognito", "brithday": "1999.03.09"}
+    {"name": "Mister Incognito", "brithday": "1999.03.12"}
 ]
+
+birthday = users_list("birthday")
 
 
 def get_upcoming_birthdays(users):
     pass
 
-print(users)
+
+for birthday in user_list:
+    birthday_date = datetime.strftime(birthday)
 
 
 
+get_upcoming_birthdays(users_list)
+today = datetime.today().date()
 
-get_upcoming_birthdays(users)
-now = datetime.today().date()
-print(now)
+print(birthday)
+print(today)
+print(users_list)
