e57f7ecaba4e2f7bafedfa36d736745d67cd620f
TechnoGoku <bulatikilya@gmail.com>
2024-03-09 20:19:56 +0200

    update_2

--- hw_4.py
+++ hw_4.py
@@ -1,7 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+users = [
+    {"name": "Joe McNuggets", "birthday": "1985.01.23"},
+    {"name": "Frensis McChicken", "birthday": "1990.01.27"},
+    {"name": "Gustav Antonio", "birthday": "1990.03.05"},
+    {"name": "Carl Johnson", "birthday":"1968.07.10"},
+    {"name": "Nico Bellic", "birthday": "1990.03.07"},
+    {"name": "Gabe Newell", "birthday": "1962.10.03"},
+    {"name": "Sir Potato", "birthday": "2000.07.16"},
+    {"name": "Mister Incognito", "brithday": "1999.03.09"}
+]
 
-now = datetime.today().date()
-print(now)
 
 def get_upcoming_birthdays(users):
     pass
+
+
+
+
+
+now = datetime.today().date()
+print(now)
